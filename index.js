05e9975f6259b04bf421c8e09e05ddb108fdf6ac
SparcerDev <developer.sparcer@gmail.com>
2017-05-28 12:52:27 +0100

    develops callback

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const https = require('https');
 
 const restService = express();
 restService.use(bodyParser.json());
 
 restService.post('/hook', function (req, res) {
-
+    var PVdict = {"date":"","time":"","energy":"","power":"","effeciency":""};
     console.log('hook request');
 
 
@@ -17,10 +17,10 @@ restService.post('/hook', function (req, res) {
 
 
 
-        if (false) {
+        if (req.body) {
             var requestBody = req.body;
 
-            if (requestBody.result) {
+            if (false) {
                 speech = '';
 
                 if (requestBody.result.fulfillment) {
@@ -36,12 +36,25 @@ restService.post('/hook', function (req, res) {
 
         console.log('result: ', speech);
 
-        restService.get('https://pvoutput.org/service/r2/getstatus.jsp?sid=43392&key=solarharvey9kwapi', function (PVresponse){
+        https.get('https://pvoutput.org/service/r2/getstatus.jsp?sid=43392&key=solarharvey9kwapi', function(PVres) {
+          PVres.setEncoding('utf8');
+          PVres.on('data', function(chunk) {
+            var PVoutput = chunk.split(',');
+            PVdict.date = PVoutput[0];
+            PVdict.time = PVoutput[1];
+            PVdict.energy = PVoutput[2];
+            PVdict.power = PVoutput[3];
+            PVdict.effeciency = PVoutput[6];
+
+            var dataIntent = requestBody.result.paramenters.PVoutputParamenter;
             return res.json({
-                speech: PVresponse,
-                displayText: PVresponse,
+                speech: "The current " + dataIntent + " output is " + PVdict[dataIntent] + " kilowatts",
+                displayText: PVdict.power + "kW, " + PVdict.energy + "kWh today,",
                 source: 'apiai-webhook-sample'
         });
+          });
+        })
+
         });
     } catch (err) {
         console.error("Can't process request", err);
