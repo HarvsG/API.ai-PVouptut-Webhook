15f8236123f2b7a9982e49c7a1b42db22486b0c3
ghjpharvey <ghjpharvey@gmail.com>
2017-06-02 16:29:44 -0400

    builds framework

--- index-googleclient.js
+++ index-googleclient.js
@@ -2,7 +2,7 @@
 //a version from scratch built using Actions on Google Client Library
 
 process.env.DEBUG = 'actions-on-google:*';
-const App = require('actions-on-google').ApiAiApp;
+const ApiAiApp = require('actions-on-google').ApiAiApp;
 
 // API.AI actions
 const UNRECOGNIZED_DEEP_LINK = 'deeplink.unknown';
@@ -24,3 +24,38 @@ const PVoutputParameter = {
   ENERGY: 'energy',
   EFFICIENCY: 'efficiency'
 };
+
+const LINK_OUT_TEXT = 'Learn more';
+const PVOUTPUT_LINK = 'https://pvoutput.org/';
+const NEXT_INFO_DIRECTIVE = 'Would you like to know anything else?';
+const CONFIRMATION_SUGGESTIONS = ['Sure', 'No thanks'];
+
+const NO_INPUTS = [
+  'I didn\'t hear that.',
+  'If you\'re still there, say that again.',
+  'We can stop here. See you soon.'
+];
+
+exports.PVoutputFullfilment = (request, response) => {
+  const app = new ApiAiApp({ request, response });
+  
+  let requestHeader = JSON.stringify(request.headers)
+  console.log('Request headers: ' + requestHeader);
+  let requestBody = JSON.stringify(request.body)
+  console.log('Request body: ' + requestBody);
+  
+  function unrecognised (app) {
+    
+  }
+  
+  function fetchInfo (app){
+    app.ask('Welcome to number echo! Say a number.');
+  }
+  
+  let actionMap = new Map();
+  actionMap.set(UNRECOGNIZED_DEEP_LINK, unrecognised);
+  actionMap.set(FETCH_INFO, fetchInfo);
+  
+  app.handleRequest(actionMap);
+}
+
