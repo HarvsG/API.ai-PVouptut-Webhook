baeccbb611dd138eccc6899363c2402f34afa513
ghjpharvey <ghjpharvey@gmail.com>
2017-07-03 15:38:31 -0400

    convert to 12 hour clock

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ exports.PVoutputFullfilment = (request, response) => {
           switch (service) {
             case "getstatus":
               PVdict.date = PVoutput[0];
-              PVdict.time = PVoutput[1];
+              PVdict.time = (parseInt(PVoutput[1].slice(0,2)) % 12) + ':' + PVoutput[1].slice(3) +
+                ( (parseInt(PVoutput[1].slice(0,2)) ) < 12 ? 'AM' : 'PM' );
               PVdict.energy = (PVoutput[2]/1000).toString();
               PVdict.power = (PVoutput[3]/1000).toString();
               PVdict.efficiency = (PVoutput[6]*100).toString();
